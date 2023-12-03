1b6333f1ec410a6ef14851d7283629f9cafed6ba
tomas193 <111932396+tomas193@users.noreply.github.com>
2023-12-03 13:34:26 -0800

    ants.py

--- ants.py
+++ ants.py
@@ -4,9 +4,7 @@ weights=[70,73,77,80,82,87,90,94,98,106,110,113,115,118,120]
 profits=[135,139,149,150,156,163,173,184,192,201,210,214,221,229,240]
 
 c=750 #knapsack capacity
-tasa_ev=0.5
 hormigas=10
-profit_backpack=0
 
 def mu(profit,weight):
     u=profit/(weight**2)
